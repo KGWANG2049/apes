882ec5a06a46ec57e38027f504008a43cb879543
wang <102914857+KGWANG2049@users.noreply.github.com>
2023-02-17 14:56:45 +0100

    Add files via upload

--- APES_2D_train.py
+++ APES_2D_train.py
@@ -14,9 +14,9 @@ from mp2d.scripts.manipulator import manipulator
 from apes.network_2d import APESCriticNet, APESGeneratorNet
 from mp2d.scripts.utilities import load_planning_req_dataset
 
-REPLAY_MAX = 10
-REPLAY_SAMPLE_SIZE = 2
-TRAIN_T = 4
+REPLAY_MAX = 4000
+REPLAY_SAMPLE_SIZE = 32
+TRAIN_T = 100
 SAVE_INTERVAL = 50
 start = time.time()
 recent_steps = []
@@ -26,13 +26,13 @@ LR = 1e-3
 LOG_ALPHA_MIN = -10.
 LOG_ALPHA_MAX = 20.
 cwd = os.getcwd()
-easy_path = "/home/wangkaige/Project/apes/easy_pl_req_250_nodes.json"
+easy_path = "/home/wang_ujsjo/Praktikum/apes/easy_pl_req_250_nodes.json"
 dataset = load_planning_req_dataset(easy_path)
 dof = 2
 links = [0.5, 0.5]
 ma = manipulator(dof, links)
 pl = Planning(ma)
-pl_req_file_name = "/home/wangkaige/Project/apes/easy_pl_req_250_nodes.json"
+pl_req_file_name = "/home/wang_ujsjo/Praktikum/apes/easy_pl_req_250_nodes.json"
 planning_requests = load_planning_req_dataset(pl_req_file_name)
 replay_buffer = deque(maxlen=REPLAY_MAX)
 writer = SummaryWriter()
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     pl_req = planning_requests[1]
     req = dataset[1]
     OC = np.array(np.shape(pl.get_occupancy_map(req)))
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0")
     gen_model = APESGeneratorNet().double().to(device)
     gen_model.eval()
     critic_model = APESCriticNet().double().to(device)
@@ -58,32 +58,20 @@ if __name__ == '__main__':
     a = torch.tensor(0.03)
 
     for i in range(0, REPLAY_MAX):
-        pl_req = planning_requests[i]
-        SV = pl_req.start
-        SV = torch.tensor(SV)
-        # print("start vektor:", SV)
-        # print("Original shape:", SV.shape)
-        GV = pl_req.goal
-        GV = torch.tensor(GV)
-        # print("goal vektor:", GV)
-        # print("Original shape:", GV.shape)
         req = dataset[i]
         OC = pl.get_occupancy_map(req)
-        OC = torch.tensor(OC)
         OC = OC.reshape([1, OC.shape[0], -1])
-        # print("occupancy map:", OC)
-        # print("Original shape:", OC.shape)
+        pl_req = planning_requests[i]
+        SV = pl_req.start
+        GV = pl_req.goal
         pl.generate_graph_halton(150)
         pr = pl.search(req)
         VALUE_ESTIMATE = pr.checked_counts
-        VALUE_ESTIMATE = torch.tensor(VALUE_ESTIMATE)
-        # print(VALUE_ESTIMATE)
-        # print("total iterations:", VALUE_ESTIMATE)
-        # print("Original shape:", VALUE_ESTIMATE.shape)
-        W = gen_model(OC, SV, GV)
-        W = torch.tensor(W)
-        # print("distribution weight:", W)
-        # print("Original shape:", W.shape)
+        VALUE_ESTIMATE = torch.tensor(VALUE_ESTIMATE).to(device)
+        OC = torch.tensor(OC).to(device)
+        SV = torch.tensor(SV).to(device)
+        GV = torch.tensor(GV).to(device)
+        W = torch.tensor(gen_model(OC, SV, GV)).to(device)
         experience = ([OC, SV, GV, W, VALUE_ESTIMATE])
         replay_buffer.append(experience)
         print('Waiting for buffer size ... {}/{}'.format(len(replay_buffer), REPLAY_MAX))

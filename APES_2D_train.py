de4e822e6724f1bcc7c5ff432b1c23bf530f13c8
wang <102914857+KGWANG2049@users.noreply.github.com>
2023-03-10 11:15:44 +0100

    Add files via upload

--- APES_2D_train.py
+++ APES_2D_train.py
@@ -19,8 +19,9 @@ from experience import plan
 from Rand_RRTCocnnect import random_plan
 from datetime import datetime
 
-BUFFER_MAX = 8
-REPLAY_SAMPLE_SIZE = 8
+
+BUFFER_MAX = 4
+REPLAY_SAMPLE_SIZE = 2
 EPOCH = 10
 LR_cri = 1e-4
 LR_gen = 1e-4
@@ -43,7 +44,7 @@ if __name__ == '__main__':
 
     SV = np.array(2)
     GV = np.array(2)
-    W = np.array(10)
+    W = np.array(50)
     data = np.array([])
     OC = np.array(np.shape(pl.get_occupancy_map(planning_requests[1])))
     device = torch.device("cuda:0")
@@ -62,8 +63,8 @@ if __name__ == '__main__':
         VALUE_RAN_SUM = 0
         VALUE_ESTIMATE_SUM = 0
         for i in range(0, BUFFER_MAX):
-            ran_idx = torch.randint(low=0, high=4000, size=(1,))
-            pl_req = planning_requests[i + ran_idx]
+            # ran_idx = torch.randint(low=0, high=4000, size=(1,))
+            pl_req = planning_requests[i]
             OC = pl.get_occupancy_map(pl_req)
             OC = OC.reshape([1, OC.shape[0], -1])
             SV = pl_req.start
@@ -90,12 +91,11 @@ if __name__ == '__main__':
             print('Current Batch', p + 1, "/", EPOCH,
                   'Waiting for buffer size ... {}/{}'.format(len(replay_buffer), BUFFER_MAX))
             print("value ran", VALUE_RAN_SUM, "value gmm", VALUE_ESTIMATE_SUM)
-
         RAN_VALUE_LIST.append(VALUE_RAN_SUM)
         VALUE_ESTIMATE_LIST.append(VALUE_ESTIMATE_SUM)
 
         # labels = sampled_oc, sampled_start_v, sampled_goal_v, sampled_coefficients, sampled_values
-        sampled_evaluations = random.sample(replay_buffer, REPLAY_SAMPLE_SIZE)
+        sampled_evaluations = random.sample(replay_buffer, BUFFER_MAX)
         # data_labels = {sampled_evaluations[i]: labels[i] for i in range(len(sampled_evaluations))}
         sampled_oc = torch.stack([t[0] for t in sampled_evaluations])
         # print("OC:", sampled_oc, sampled_oc.shape)
@@ -109,77 +109,85 @@ if __name__ == '__main__':
         # print("value:", sampled_values, sampled_values.shape)
 
         # update Cir
-        critic_loss = 0
 
-        for j in range(REPLAY_SAMPLE_SIZE):
-            mean, std = critic_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j], sampled_coefficients[j])
-            std = torch.exp(std)
-            # print("mean:", mean, "std:", std)
-            priori_pro = dist.Normal(mean, std)
-            # print("posterior:", priori_pro)
-            posterior_prob = priori_pro.log_prob(sampled_values[j])
-            # print("posterior_prob:", posterior_prob)
-            # print("test", priori_pro.log_prob(a).exp())
-            critic_loss = (critic_loss + (-posterior_prob))/REPLAY_SAMPLE_SIZE
+        for i in range(20):
+            critic_loss = 0
+            for j in range(REPLAY_SAMPLE_SIZE):
+                sam_idx = j+REPLAY_SAMPLE_SIZE*i
+                mean, std = critic_model(sampled_oc[sam_idx], sampled_start_v[sam_idx], sampled_goal_v[sam_idx], sampled_coefficients[sam_idx])
+                std = torch.exp(std)
+                # print("mean:", mean, "std:", std)
+                priori_pro = dist.Normal(mean, std)
+                # print("posterior:", priori_pro)
+                posterior_prob = priori_pro.log_prob(sampled_values[j])
+                # print("posterior_prob:", posterior_prob)
+                # print("test", priori_pro.log_prob(a).exp())
+                critic_loss = (critic_loss + (-posterior_prob))/REPLAY_SAMPLE_SIZE
 
-        critic_model_optimizer.zero_grad()
-        critic_loss.backward(retain_graph=True)
-        torch.nn.utils.clip_grad_norm_(critic_model.parameters(), 1.0)
-        critic_model_optimizer.step()
+            critic_model_optimizer.zero_grad()
 
-        # Update generator
-        gen_objective = 0
-        for k in range(REPLAY_SAMPLE_SIZE):
-            mean = critic_model(sampled_oc[k], sampled_start_v[k], sampled_goal_v[k], sampled_coefficients[k])[0]
-            # print("mean", mean)
-            # dir_dist = Dirichlet(sampled_coefficients[k])
-            entropy = gen_model(sampled_oc[k], sampled_start_v[k], sampled_goal_v[k]).entropy()
-            # print("entropy", entropy)
-            dual_terms = (log_alpha.exp().detach() * entropy)
-            # print("dual_term", dual_terms)
-            gen_objective = gen_objective + mean - dual_terms
-            # gen_objective = gen_objective + mean
+            critic_loss.backward(retain_graph=True)
+            torch.nn.utils.clip_grad_norm_(critic_model.parameters(), 1.0)
+            critic_model_optimizer.step()
 
-        # print("gen_objectivesum", gen_objective)
-        # critic_model_optimizer.zero_grad()
-        # gen_model_optimizer.zero_grad()
-        gen_model_optimizer.zero_grad()
-        gen_objective.backward()
-        torch.nn.utils.clip_grad_norm_(gen_model.parameters(), 1.0)
-        gen_model_optimizer.step()
-        print("critic_loss", critic_loss)
-        print("gen_objective", gen_objective)
+            # Update generator
+            gen_objective = 0
+            for k in range(REPLAY_SAMPLE_SIZE):
+                sam_idx = k + REPLAY_SAMPLE_SIZE * i
+                print("1212121111112121", sam_idx)
+                mean = critic_model(sampled_oc[sam_idx], sampled_start_v[sam_idx], sampled_goal_v[sam_idx], sampled_coefficients[sam_idx])[0]
+                # print("mean", mean)
+                # dir_dist = Dirichlet(sampled_coefficients[k])
+                entropy = gen_model(sampled_oc[sam_idx], sampled_start_v[sam_idx], sampled_goal_v[sam_idx]).entropy()
+                # print("entropy", entropy)
+                dual_terms = (log_alpha.exp().detach() * entropy)
+                # print("dual_term", dual_terms)
+                gen_objective = (gen_objective + mean - dual_terms)/REPLAY_SAMPLE_SIZE
+                # gen_objective = gen_objective + mean
 
-        # update alpha
-        alpha_loss = 0
-        for j in range(REPLAY_SAMPLE_SIZE):
-            # dir_dist = Dirichlet(sampled_coefficients[j])
-            entropy = gen_model(sampled_oc[j], sampled_start_v[j], sampled_goal_v[j]).entropy()
-            print("entropy", entropy)
-            alpha_loss_single = log_alpha.exp() * ((entropy - torch.tensor(
-                TARGET_ENTROPY, device=device, dtype=torch.float32)).detach())
-            #  print("1", alpha_loss_single)
-            alpha_loss = alpha_loss + alpha_loss_single
-        # print("2", alpha_loss)
-        # print("3", alpha_loss)
-        alpha_optim.zero_grad()
-        alpha_loss.backward()
-        with torch.no_grad():
-            log_alpha.grad *= (((-log_alpha.grad >= 0) | (log_alpha >= LOG_ALPHA_MIN)) &
-                               ((-log_alpha.grad < 0) | (log_alpha <= LOG_ALPHA_MAX))).float()  # ppo
-        alpha_optim.step()
+            # print("gen_objectivesum", gen_objective)
+            # critic_model_optimizer.zero_grad()
+            # gen_model_optimizer.zero_grad()
 
-        # critic_loss = critic_loss.detach().numpy()
-        """critic_loss = critic_loss.item()
-        gen_objective = gen_objective.item()
-        alpha_loss = alpha_loss.item()"""
+            gen_model_optimizer.zero_grad()
+            gen_objective.backward(retain_graph=True)
+            torch.nn.utils.clip_grad_norm_(gen_model.parameters(), 1.0)
+            gen_model_optimizer.step()
+            print("critic_loss", critic_loss)
+            print("gen_objective", gen_objective)
 
-        # loss function visualable
+            # update alpha
+            alpha_loss = 0
+            for m in range(REPLAY_SAMPLE_SIZE):
+                sam_idx = m + REPLAY_SAMPLE_SIZE * i
+                # dir_dist = Dirichlet(sampled_coefficients[j])
+                entropy = gen_model(sampled_oc[sam_idx], sampled_start_v[sam_idx], sampled_goal_v[sam_idx]).entropy()
+                print("entropy", entropy)
+                alpha_loss_single = log_alpha.exp() * ((entropy - torch.tensor(
+                    TARGET_ENTROPY, device=device, dtype=torch.float32)).detach())
+                #  print("1", alpha_loss_single)
+                alpha_loss = (alpha_loss + alpha_loss_single)/REPLAY_SAMPLE_SIZE
+            # print("2", alpha_loss)
+            # print("3", alpha_loss)
+            alpha_optim.zero_grad()
+            alpha_loss.backward()
+            with torch.no_grad():
+                log_alpha.grad *= (((-log_alpha.grad >= 0) | (log_alpha >= LOG_ALPHA_MIN)) &
+                                   ((-log_alpha.grad < 0) | (log_alpha <= LOG_ALPHA_MAX))).float()  # ppo
+            alpha_optim.step()
 
-        writer.add_scalar("CRITIC LOSS", critic_loss, p)
-        writer.add_scalar("GEN LOSS", gen_objective, p)
-        # writer.add_scalar("ALPHA LOSS", alpha_loss, p)
-    writer.close()
+            # critic_loss = critic_loss.detach().numpy()
+            critic_loss = critic_loss.item()
+            gen_objective = gen_objective.item()
+            alpha_loss = alpha_loss.item()
+
+            # loss function visualable
+
+            writer.add_scalar("CRITIC LOSS", critic_loss)
+            writer.add_scalar("GEN LOSS", gen_objective)
+            # writer.add_scalar("ALPHA LOSS", alpha_loss, p)
+        writer.close()
+
     # Compare result visualization
     plt.plot(RAN_VALUE_LIST, color="red")
     plt.plot(VALUE_ESTIMATE_LIST, color="green")  # Use Tensor.cpu() to copy the tensor to host memory first.
@@ -197,3 +205,6 @@ if __name__ == '__main__':
     # tensorboard --logdir /home/wang_ujsjo/Praktikum/apes/Loss_Function
 
 # modified can be saved 08.03,23
+# RuntimeError: one of the variables needed for gradient computation has been modified by an inplace operation: [torch.cuda.DoubleTensor [200, 50]], which is output 0 of AsStridedBackward0, is at version 2; expected version 1 instead. Hint: enable anomaly detection to find the operation that failed to compute its gradient, with torch.autograd.set_detect_anomaly(True).
+
+# Process finished with exit code 1
